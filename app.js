8f23b210edb0a8754b3b1d513ad76c061837a1d7
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2021-05-02 19:36:47 +0530

    commit all CRUD

--- app.js
+++ app.js
@@ -1,58 +1,133 @@
 //jshint esversion:6
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+const mongoose = require('mongoose');
 
-// Database Name
-const dbName = 'fruitsDB';
+mongoose.connect("mongodb://localhost:27017/fruitsDB",  { useNewUrlParser: true , useUnifiedTopology: true });
 
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function() {
-      client.close();
-    });
+const fruitSchema = new mongoose.Schema ({
+  name:{
+    type:String,
+    required:[true, "Please check youe data entry, no name specified!"]
+  },
+  rating :{
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review : String
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-        name : "Apple",
-        score : 8,
-        review : "Great fruit"
-    },
-      {
-        name : "Orange",
-        score : 6,
-        review : "Kinda sour"
-    },
-      {
-        name : "Banana",
-        score : 9,
-        review : "Great stuff!"
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
+//Mongoose model
 
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  rating: 10,
+  review: "Peeches are so yummy!."
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema
+});
+
+//Mongoose model
+
+const Person = mongoose.model("Person", personSchema);
+
+const mango = new Fruit({
+  name:"Mango",
+  score:9,
+  review:"Greate Fruit"
+});
+
+mango.save();
+
+Person.updateOne({name:"John"},{favouriteFruit:mango},function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("Successfully updated!");
+  }
+});
+// const person = new Person({
+//   name:"Amy",
+//   age:7,
+//   favouriteFruit:pineapple
+// });
+
+//person.save();
+
+// const kiwi = new Fruit({
+//   name :"Kiwi",
+//   rating:10,
+//   review:"The best fruit"
+// });
+// const orange = new Fruit({
+//   name :"Orange",
+//   rating:7,
+//   review:"The best fruit"
+// });
+// const banana = new Fruit({
+//   name :"Banana",
+//   rating:5,
+//   review:"The best fruit"
+// });
+
+// Fruit.insertMany([kiwi,orange,banana], function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//     else{
+//       console.log("Successfully saved");
+//     }
+// });
+//reading from from database with mongoose
+Fruit.find(function(err,fruits){
+  if(err){
+    console.log(err);
+  }
+    else{
+      mongoose.connection.close();
+      fruits.forEach(function(fruit){
+        console.log(fruit.name);
+      });
+      
+    }
+
+});
+//fruit.save();
+
+// Fruit.updateOne({_id:"608e66fd2ed4040b3808606d"}, {name : "Peach"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("Successfully updated!");
+//   }
+// });
+
+// Fruit.deleteOne({name:"Peach"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("Successfully deleted!");
+//   }
+
+// });
+
+// Person.deleteMany({name:"John"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("Successfully deleted");
+//   }
+// });
 const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('fruits');
